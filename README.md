ef4ea5dc449d0a14e076a22b94551a7c81bd6865
SnootierMoon <aku24.7x3@gmail.com>
2021-11-21 17:14:20 -0800

    Switch to SDL and Create test window

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pictionary
+# Draw Plus Plus
 
 An app written C++ that lets you play pictionary with your friends over the
 internet
@@ -8,7 +8,8 @@ internet
 ### Libraries for Building
 
 - [CMake](https://cmake.org/install/)
-- [SFML](https://www.sfml-dev.org/download/sfml/2.5.1/) v2.5
+- [raylib](https://github.com/raysan5/raylib) (downloaded automatically while building)
+- [nbnet](https://github.com/nathhB/nbnet) (downloaded automatically while building)
 
 ### Build
 
@@ -18,3 +19,7 @@ internet
 ### Running
 
 After building, the executable gets created in `/build/`.
+
+Currently, it's just a line-drawing test application. After running it, you can
+draw lines by holding the left mouse button and dragging the mouse around the
+screen.
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,78 @@
-#include <SFML/Graphics.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_framerate.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <iostream>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+bool closed = false;
+int thickness = 5;
 
 int main() 
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    while (window.isOpen())
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 400, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    FPSmanager fps;
+    SDL_initFramerate(&fps);
+    SDL_setFramerate(&fps, 60);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    while (!closed)
     {
-        sf::Event event;
-        while (window.pollEvent(event)) 
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == SDL_QUIT)
+            {
+                closed = true;
+            }
+            else if (event.type == SDL_MOUSEMOTION)
+            {
+                SDL_MouseMotionEvent motion = event.motion;
+                bool is_motion = motion.xrel || motion.yrel;
+                bool left_mouse_down = SDL_GetMouseState(NULL, NULL) & SDL_BUTTON_LMASK;
+                if (is_motion && left_mouse_down)
+                {
+                    thickLineRGBA(
+                            renderer,
+                            motion.x - motion.xrel, motion.y - motion.yrel,
+                            motion.x, motion.y,
+                            thickness * 2,
+                            0, 0, 0, 255
+                    );
+                    filledCircleRGBA(
+                            renderer,
+                            motion.x - motion.xrel, motion.y - motion.yrel,
+                            thickness,
+                            0, 0, 0, 255
+                    );
+                    filledCircleRGBA(
+                            renderer,
+                            motion.x, motion.y,
+                            thickness,
+                            0, 0, 0, 255
+                    );
+                }
+            }
+            else if (event.type == SDL_MOUSEWHEEL)
+            {
+                thickness += event.wheel.y;
+                thickness = std::min(std::max(thickness, 0), 30);
+            }
         }
-        window.clear();
-        window.display();
+
+        SDL_RenderPresent(renderer);
+        SDL_framerateDelay(&fps);
     }
-    return EXIT_SUCCESS;
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
